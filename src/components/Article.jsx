aed372b23e6d217f197e2e88f66a8241d94575fb
DanielTaylor1998 <dt.info.1998@gmail.com>
2022-09-27 14:17:53 +0100

    added viewing a single  article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,12 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getArticle } from "../utils/api";
+import "./Article.css"
 
 export const Article = () => {
 
     const { article_id } = useParams();
     const [currentArticle, setCurrArticle] = useState({})
+    const [loading, Isloading] = useState(true)
+
+    useEffect(() => {
+        getArticle(article_id)
+            .then((article) => {
+                setCurrArticle(article);
+                Isloading(false);
+            })
+    }, [])
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
 
     return (
-        <h1>Got here !</h1>
+        <div className="article">
+            <div className="col"></div>
+            
+            <div className="Content">
+                <h2>{currentArticle.author}</h2>
+                <h1>{currentArticle.title}</h1>
+                <br />
+                <p>{currentArticle.body}</p>
+                <></>
+            </div>
+
+            <div className="col"></div>
+        </div>
+
     )
 }
