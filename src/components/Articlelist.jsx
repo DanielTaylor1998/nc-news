331e3bdeda07b5a207ea4d819da5bc4879b0841e
DanielTaylor1998 <dt.info.1998@gmail.com>
2022-09-30 09:20:28 +0100

    refactored code for useSearchParams

--- src/components/Articlelist.jsx
+++ src/components/Articlelist.jsx
@@ -1,23 +1,24 @@
 import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { getArticles } from "../utils/api";
 import "./Articlelist.css"
 
-export const Articlelist = (topic) => {
+export const Articlelist = ({ topics }) => {
 
     const navigate = useNavigate()
-
     const [articles, setArticles] = useState([])
     const [isLoading, SetIsLoading] = useState(true)
+    const [searchParams, setSearchParams] = useSearchParams();
 
     useEffect(() => {
-        getArticles(topic.topic)
+        getArticles(searchParams)
             .then((articles) => {
                 setArticles(articles);
                 SetIsLoading(false)
             })
 
-    }, [topic])
+    }, [searchParams])
 
 
     if (isLoading) {
@@ -26,10 +27,28 @@ export const Articlelist = (topic) => {
 
     return (
         <div>
+            <div className="topics">
+                {topics.map((topic) => {
+                    return (
+
+                        <button key={topic.slug} className="button" onClick={() => {
+                            setSearchParams({ topic: topic.slug })
+                        }}>{topic.slug}</button>
+
+                    )
+                })}
+                <button className="button" onClick={() => {
+                    setSearchParams({})
+                }}>All</button>
+                <br />
+                <button >Sort By</button>
+                <br />
+
+            </div>
             {articles.map((article) => {
                 return (
                     <div key={article.article_id}>
-                        <div className="Article-Card" onClick={() => {navigate(`/Articles/${article.article_id}`)}}>
+                        <div className="Article-Card" onClick={() => { navigate(`/Articles/${article.article_id}`) }}>
                             <h1 className="title">{article.title}</h1>
                         </div>
                         <br />
